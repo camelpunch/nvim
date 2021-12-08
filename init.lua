064cb998506c3202cb279cb4e0f981aa7c31fc01
Andrew Bruce <me@andrewbruce.net>
2021-12-08 09:23:12 +0000

    <leader>a greps for string under cursor

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@ vim.api.nvim_set_keymap('n', '<space>', ':nohlsearch<cr>', {noremap = true})
 
 -- telescope
 vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>a', '<cmd>Telescope grep_string<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {noremap = true})
