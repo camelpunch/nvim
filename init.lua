5c1beae6a308ca2effadd663cb83483ba0579df1
Andrew Bruce <me@andrewbruce.net>
2022-12-09 18:00:53 +0000

    Configure rust-analyzer to support e.g. dev

--- init.lua
+++ init.lua
@@ -92,4 +92,22 @@ lsp.tailwindcss.setup {}
 
 lsp.rust_analyzer.setup {
   on_attach = on_attach,
+  settings = {
+    ["rust-analyzer"] = {
+      imports = {
+        granularity = {
+          group = "module",
+        },
+        prefix = "self",
+      },
+      cargo = {
+        buildScripts = {
+          enable = true,
+        },
+      },
+      procMacro = {
+        enable = true
+      },
+    }
+  }
 }
