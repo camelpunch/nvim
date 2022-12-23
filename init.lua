1b8232aaecfddb25a6ff17f737aab67c504c9ce8
Andrew Bruce <me@andrewbruce.net>
2022-12-23 14:35:16 +0000

    mapping for LSP rename

--- init.lua
+++ init.lua
@@ -68,6 +68,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
+  buf_set_keymap('n', '<leader><F6>', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 end
 
 lsp.elixirls.setup {
